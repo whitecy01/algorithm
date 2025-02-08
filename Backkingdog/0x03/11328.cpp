2635b12a2833fa18d3a1d7e83aec1ad1e734017b
whitecy01 <whitecy01@naver.com>
2025-02-08 23:20:22 +0900

    fin 0x03

--- Backkingdog/0x03/11328.cpp
+++ Backkingdog/0x03/11328.cpp
@@ -4,8 +4,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    string a,b;
+    int arr[26] = {0};
+    int brr[26] = {0};
+    cin >> a >> b;
+    if (a.size() != b.size())
+    {
+      cout << "Impossible\n";
+      continue;
+    }
+    for (int i = 0; i < a.size(); i++)
+    {
+      arr[a[i] - 'a']++;
+    }
+    for (int i = 0; i < b.size(); i++)
+    {
+      brr[b[i] - 'a']++;
+    }
+    bool flag = false;
+    for (int i = 0; i < 26; i++)
+    {
+      if (arr[i] != brr[i]){
+        cout << "Impossible\n";
+        flag = true;
+        break;
+      }
+    }
+    if (flag == false)
+      cout << "Possible\n";
+  }
 }
--- Backkingdog/0x03/1919.cpp
+++ Backkingdog/0x03/1919.cpp
@@ -4,8 +4,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int arr[26] = {0};
+int brr[26] = {0};
+
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
+
+  string a,b;
+  cin >> a >> b;
+  for (int i = 0; i < a.size(); i++)
+  {
+    arr[a[i] - 'a']++; 
+  }
+  for (int i = 0; i < b.size(); i++)
+  {
+    brr[b[i] - 'a']++; 
+  }
+  int ans = 0;
+  for (int i = 0; i < 26; i++)
+  {
+    int temp = 0;
+    if (arr[i] > brr[i]){
+      temp = arr[i] - brr[i];
+      // cout << "first i : " << i << "\n";
+    }
+    else if (arr[i] < brr[i]){
+      temp = brr[i] - arr[i];
+      // cout << "seocnd i : " << i << "\n";
+    }
+    ans += temp;
+  }
+  cout << ans << "\n";
+  
   
 }
