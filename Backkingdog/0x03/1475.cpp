bf27027cdf835f689ab0db5a9b5c5484a878ba0c
whitecy01 <whitecy01@naver.com>
2025-02-08 12:00:46 +0900

    0x03

--- Backkingdog/0x03/1475.cpp
+++ Backkingdog/0x03/1475.cpp
@@ -4,8 +4,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int arr[10];
+
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
+
+  int n;
+  cin >> n;
+  fill(arr, arr + 9, 0);
+  while (n)
+  {
+    int temp = n % 10;
+    arr[temp]++;
+    n = n / 10;
+  }
+
+  int temp2 = 0;
+  int temp = (arr[6] + arr[9] + 1) / 2;
+  arr[6] = arr[9] = temp;
+  //   for (int i = 0; i < 10; i++)
+  // {
+  //   cout << arr[i] << " ";
+  // }
+  // cout << "\n";
+  for (int i = 0; i < 10; i++)
+  {
+    temp2 = max(temp2, arr[i]);
+  }
+  cout << temp2 << "\n";
+  
+  
   
 }
--- Backkingdog/0x03/2577.cpp
+++ Backkingdog/0x03/2577.cpp
@@ -4,8 +4,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int arr[10];
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
+  int a,b,c;
+
+  cin >> a >> b >> c;
+  long long answer = a * b * c;
+  // cout << "answer : " << answer << "\n";
+  fill(arr, arr+10, 0);
+  while (answer){
+    int temp = answer % 10;
+    // cout <<  "temp : "  << temp << "\n";
+    arr[temp]++;
+    answer = answer / 10;
+  }
+  for (int i = 0; i < 10; i++)
+  {
+    cout << arr[i] << '\n';
+  }
+  
   
 }
