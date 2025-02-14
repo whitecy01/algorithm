9e18fbda494ea77aa81e71422d1264cc05e4b405
whitecy01 <whitecy01@naver.com>
2025-02-14 21:36:18 +0900

    스택, 큐, 덱 기본 문제 BFS 응용 문제 한개

--- Backkingdog/0x07/1021.cpp
+++ Backkingdog/0x07/1021.cpp
@@ -1,11 +1,54 @@
 // Authored by : whitecy
 // Co-authored by : -
-// http://boj.kr/****************
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
+
+  int n, m;
+  cin >> n >> m;
+  deque<int> D;
+  for (int i = 1; i <= n; i++)
+  {
+    D.push_back(i);
+  }
+  deque<int>::iterator it = D.begin(); 
+  int ans = 0;
+  for (int i = 0; i < m; i++)
+  {
+    int num;
+    cin >> num;
+    int idx = find(D.begin(), D.end(), num) - D.begin();
+    // cout << "num : " << num << " idx : " << idx << "\n"; 
+
+    int left = idx;
+    int right = n - idx;
+    // cout << "left : " << left << " right : " << right << "\n"; 
+
+    if (left < right){
+      ans += left;
+      while (left--)
+      {
+        D.push_back(D.front());
+        D.pop_front();
+      }
+      D.pop_front();
+      n--;
+    }
+    else {
+      ans += right;
+      while (right--)
+      {
+        D.push_front(D.back());
+        D.pop_back();
+      }
+      D.pop_front();
+      n--;
+    }
+  }
+  cout << ans << "\n";
   
 }
--- Backkingdog/0x07/10866.cpp
+++ Backkingdog/0x07/10866.cpp
@@ -1,11 +1,66 @@
 // Authored by : whitecy
 // Co-authored by : -
-// http://boj.kr/****************
-#include <bits/stdc++.h>
+
+#include <iostream>
+#include <deque>
+
 using namespace std;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
+int main(){
+    int n;
+
+    cin >> n;
+    string str;
+    deque<int> de;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> str;
+        if (str == "push_front")
+        {
+            int num;
+            cin >> num;
+            de.push_front(num);
+        }
+        else if (str == "push_back"){
+            int num;
+            cin >> num;
+            de.push_back(num);
+        }
+        else if (str == "pop_front"){
+            if (!de.empty()) 
+            {
+                cout << de.front() << "\n";
+                de.pop_front();
+            }
+            else 
+                cout << "-1\n";
+        }
+        else if (str == "pop_back"){
+            if (!de.empty()) 
+            {
+                cout << de.back() << "\n";
+                de.pop_back();
+            }
+            else 
+                cout << "-1\n";
+        }
+        else if (str == "size") { cout << de.size() << "\n";}
+        else if (str == "empty") { cout << de.empty() << "\n";}
+        else if (str == "front") { 
+            if (!de.empty()) 
+            {
+                cout << de.front() << "\n";
+            }
+            else 
+                cout << "-1\n";
+        }
+        else if (str == "back"){
+            if (!de.empty()) 
+                cout << de.back() << "\n";
+            else 
+                cout << "-1\n";
+        }
+        
+    }
+    
 }
